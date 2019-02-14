b79d7b1365f1c32540e9a632d469899a372e14af
MasahiroTakahashi <take1931032@gmail.com>
2019-02-14 22:24:09 +0900

    初期盤面

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,19 +3,20 @@
     <p>othello</p>
     <table border="9" cellspacing="0">
       <tr v-for="i in y" v-bind:key="i">
-        <td v-for="j in x" v-bind:key="j">
-          <span v-if="table[i][j] == '黒'">
-            {{ table[i][j] }}
+        <td v-for="j in x" v-bind:key="j" class="cell">
+          <span v-if="table[i][j] == '黒'" class='black' >
           </span >
-          <span v-if="table[i][j] == '白'">
-            {{ table[i][j] }}
+          <span v-if="table[i][j] == '白'" class="white">
           </span>
-          <span v-if="table[i][j] == '空白'">
-            {{ table[i][j] }}
+          <span v-if="table[i][j] == '黒置'" v-on:click="canput" class="Canput">
+          </span>
+          <span v-if="table[i][j] == '空白'" class="space" >
+            <!-- {{ table[i][j] }} -->
           </span>
         </td>
       </tr>
     </table>
+    <p>{{ message }}</p>
     <router-link to="/about"></router-link>
   </div>
 </template>
@@ -24,20 +25,23 @@
 const table_y = 8;
 const table_x = 8;
 const array_table = [['','','','','','','','',''],['','','','','','','','',''],['','','','','','','','',''],['','','','','','','','',''],['','','','','','','','',''],['','','','','','','','',''],['','','','','','','','',''],['','','','','','','','',''],['','','','','','','','','']];
-
+var msg = ''
 export default {
   data () {
     return{
       y: table_y,
       x: table_x,
-      table: array_table
+      table: array_table,
+      message: msg
     }
   },
   created: function(){
     this.isinit();
     this.mytarn();
   },
-    computed: {
+    mounted: function(){
+      this.canput();
+      this.whetherCanPut();
     },
     methods: {
       isinit() {
@@ -49,12 +53,52 @@ export default {
               array_table[i][j] =  '白'
             }else{
               array_table[i][j] =  "空白"
+              // 置けるかどうか判別してarray_tableに入れとくか
             }
           }
         }
+      },
+      canput: function () {
+
+      },
+      whetherCanPut: function () {
+        // 置ける位置かどうか判別
+        return true
       },
       mytarn(){
       }
     }
 }
 </script>
+<style>
+.cell{
+  height: 50px;
+  width: 50px;
+  padding: auto;
+  background-color: aqua;
+}
+.black{
+  height: 45px;
+  width: 45px;
+  border-radius: 50%;
+  background-color: black;
+  display: inline-block;
+}
+.white{
+  height: 45px;
+  width: 45px;
+  border-radius: 50%;
+  background-color: white;
+  display: inline-block;
+}
+.space{
+  border-radius: 50%;
+}
+.Canput{
+  background-color:wheat;
+  display: inline-block;
+  /* height: 45px; */
+  /* width: 45px; */
+  border-radius: 50%;
+}
+</style>
