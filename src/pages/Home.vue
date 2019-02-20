b67341911dfa3f40fab5f5f7a241909575433b3c
MasahiroTakahashi <take1931032@gmail.com>
2019-02-20 23:50:51 +0900

    バインディングうまくいかない

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -10,12 +10,12 @@
           <span v-if="table[i][j] == '白'" class="white">
             <!-- {{ j }}{{ i }} -->
           </span>
-          <span v-if="whetherCanPut(j, i, '黒') == true && table[i][j] == '空白'" class="Canput">
+          <button v-if="whetherCanPut(j, i, '黒') == true && table[i][j] == ''" v-on:click="putStone(i,j)" class="Canput">
             <!-- クリックイベント追加 -->
-            <!-- {{ j }}{{ i }} -->
-          </span>
-          <span v-if="whetherCanPut(j, i, '黒') == false && table[i][j] == '空白'" class="space" >
-            <!-- {{ j }}{{ i }} -->
+            {{ table[i][j] }}
+          </button>
+          <span v-if="whetherCanPut(j, i, '黒') == false && table[i][j] == ''" class="space" >
+            {{ j }}{{ i }}
           </span>
         </td>
       </tr>
@@ -26,15 +26,14 @@
 </template>
 
 <script>
-const table_y = 8;
-const table_x = 8;
+
 const array_table = [['','','','','','','','',''],['','','','','','','','',''],['','','','','','','','',''],['','','','','','','','',''],['','','','','','','','',''],['','','','','','','','',''],['','','','','','','','',''],['','','','','','','','',''],['','','','','','','','','']];
 var msg = ''
 export default {
   data () {
     return{
-      y: table_y,
-      x: table_x,
+      y: 8,
+      x: 8,
       table: array_table,
       message: msg
     }
@@ -42,159 +41,159 @@ export default {
   created: function(){
     this.isinit();
   },
-    mounted: function(){
-      this.canput();
+  methods: {
+    isinit() {
+      for(var i=0;i < array_table.length; i++ ){
+        for(var j=0; j < array_table[i].length; j++){
+          if ( ( i==4 && j ==4 ) || ( i==5 &&j==5 ) ){
+            array_table[i][j] =  '黒'
+          }else if( (i==4 && j == 5) || (i == 5 && j==4 )){
+            array_table[i][j] =  '白'
+          }else{
+            array_table[i][j] =  ""
+          }
+        }
+      }
     },
-    methods: {
-      isinit() {
-        for(var i=0;i < array_table.length; i++ ){
-          for(var j=0; j < array_table[i].length; j++){
-            if ( ( i==4 && j ==4 ) || ( i==5 &&j==5 ) ){
-              array_table[i][j] =  '黒'
-            }else if( (i==4 && j == 5) || (i == 5 && j==4 )){
-              array_table[i][j] =  '白'
-            }else{
-              array_table[i][j] =  "空白"
-            }
-          }
-        }
-      },
-      canput: function () {
-      },
-      whetherCanPut: function (i, j, tarn) {
-        // 自分の位置と周辺(八方向)を判別して相手コマ(白なら黒、黒なら白)がある場合
-        // その方向を調べて行って自分と同じ色がある場合はCanPut
-        if (this.stoneReplaceLeftTop(i, j, tarn) ||
-            this.stoneReplaceTop(i, j ,tarn) ||
-            this.stoneReplaceRightTop(i, j, tarn) ||
-            this.stoneReplaceLeft(i, j, tarn) ||
-            this.stoneReplaceRight(i, j, tarn)||
-            this.stoneReplaceDown(i, j, tarn) ||
-            this.stoneReplaceLeftDown(i, j, tarn) ||
-            this.stoneReplaceRightDown(i, j, tarn) ){
-          return true
-        }
-        return false
-      },
-      stoneReplaceLeftTop(x, y, myStone){
-        const enemy = myStone == "黒" ? "白" : "黒"
-        // x=1 y=1
-        if( x == 1 || y == 1 || array_table[x-1][y-1] != enemy){
-          return false
-        }
-        var i = x
-        for(var j = y ; i > 0; i--){
-          if(array_table[i][j] == myStone ){
-            return true
-          }else if(i == 1 ){
-            return false
-          }
-          i--;
-        }
-        return false
-      },
-      stoneReplaceTop(x, y ,myStone){
-        const enemy = myStone == "黒" ? "白" : "黒"
-        // x=1 y=1~8
-        if( y == 1 || array_table[x][y-1] != enemy){
-          return false
-        }
-        for(var i = y ; i > 0; i--){
-          if(array_table[x][i] == myStone ){
-            return true
-          }
-        }
-        return false
-      },
-      stoneReplaceRightTop(x, y, myStone){
-        const enemy = myStone == "黒" ? "白": "黒"
-        // x =1 y = 8
-        if(x == 8 || y == 1 || array_table[x+1][y-1] != enemy){
-          return false
-        }
-        var i = x;
-        for(var j = y ; j >0 ; j--){
-          if(array_table[i][j] == myStone){
-            return true
-          }else if(i == 8){
-            return false
-          }
-          i++;
-        }
-        return false
-      },
-      stoneReplaceLeft(x, y, myStone){
-        const enemy = myStone == "黒" ? "白" : "黒"
-        // y=1
-        if(x == 1 || array_table[x-1][y] != enemy ){
-          return false
-        }
-        for(var i = x; i>0; i--){
-          if(array_table[i][y] == myStone){
-            return true
-          }
-        }
-        return false
-      },
-      stoneReplaceRight(x, y, myStone){
-        const enemy = myStone == "黒" ? "白" : "黒"
-        // y=8
-        if(x == 8 || array_table[x+1][y] != enemy){
-          return false
-        }
-        for(var i = x; i < 9 ; i++ ){
-          if(array_table[i][y] == myStone){
-            return true
-          }
-        }
-        return false
-      },
-      stoneReplaceLeftDown(x, y, myStone){
-        const enemy = myStone == "黒" ? "白" : "黒"
-        if(x == 1 || y == 8 || array_table[x-1][y+1] != enemy ){
-          return false
-        }
-        var i = x;
-        for(var j = y ; j < 9 ; j++ ){
-          if(array_table[i][j] == myStone ){
-            return true
-          }else if(i ==1 ){
-            return false
-          }
-          i--;
-        }
-        return false
-      },
-      stoneReplaceDown(x, y, myStone){
-        const enemy = myStone == "黒" ? "白" : "黒"
-        // x = 8
-        if(y == 8 || array_table[x][y+1] != enemy){
-          return false
-        }
-        for(var i = y; i < 9; i++){
-          if(array_table[x][i] == myStone){
-            return true
-          }
-        }
-        return false
-      },
-      stoneReplaceRightDown(x, y, myStone){
-        const enemy = myStone == "黒" ? "白" : "黒"
-        // x=8 y=8
-        if(x == 8 || y == 8 || array_table[x+1][y+1] != enemy ){
-          return false
-        }
-        var i = x;
-        for(var j = y ; j < 9 ; j++ ){
-          if(array_table[i][j] == myStone){
-            return true
-          }else if(i == 8){
-            return false
-          }
-          i++;
-        }
+    putStone(i, j){
+      // array_table[i][j] = "黒"
+      // this.table = array_table
+      this.table[i][j] = "黒"
+    },
+    whetherCanPut: function (i, j, tarn) {
+      // 自分の位置と周辺(八方向)を判別して相手コマ(白なら黒、黒なら白)がある場合
+      // その方向を調べて行って自分と同じ色がある場合はCanPut
+      if (this.stoneReplaceLeftTop(i, j, tarn) ||
+          this.stoneReplaceTop(i, j ,tarn) ||
+          this.stoneReplaceRightTop(i, j, tarn) ||
+          this.stoneReplaceLeft(i, j, tarn) ||
+          this.stoneReplaceRight(i, j, tarn)||
+          this.stoneReplaceDown(i, j, tarn) ||
+          this.stoneReplaceLeftDown(i, j, tarn) ||
+          this.stoneReplaceRightDown(i, j, tarn) ){
+        return true
+      }
+      return false
+    },
+    stoneReplaceLeftTop(x, y, myStone){
+      const enemy = myStone == "黒" ? "白" : "黒"
+      // x=1 y=1
+      if( x == 1 || y == 1 || array_table[x-1][y-1] != enemy){
         return false
       }
+      var i = x
+      for(var j = y ; i > 0; i--){
+        if(array_table[i][j] == myStone ){
+          return true
+        }else if(i == 1 ){
+          return false
+        }
+        i--;
+      }
+      return false
+    },
+    stoneReplaceTop(x, y ,myStone){
+      const enemy = myStone == "黒" ? "白" : "黒"
+      // x=1 y=1~8
+      if( y == 1 || array_table[x][y-1] != enemy){
+        return false
+      }
+      for(var i = y ; i > 0; i--){
+        if(array_table[x][i] == myStone ){
+          return true
+        }
+      }
+      return false
+    },
+    stoneReplaceRightTop(x, y, myStone){
+      const enemy = myStone == "黒" ? "白": "黒"
+      // x =1 y = 8
+      if(x == 8 || y == 1 || array_table[x+1][y-1] != enemy){
+        return false
+      }
+      var i = x;
+      for(var j = y ; j >0 ; j--){
+        if(array_table[i][j] == myStone){
+          return true
+        }else if(i == 8){
+          return false
+        }
+        i++;
+      }
+      return false
+    },
+    stoneReplaceLeft(x, y, myStone){
+      const enemy = myStone == "黒" ? "白" : "黒"
+      // y=1
+      if(x == 1 || array_table[x-1][y] != enemy ){
+        return false
+      }
+      for(var i = x; i>0; i--){
+        if(array_table[i][y] == myStone){
+          return true
+        }
+      }
+      return false
+    },
+    stoneReplaceRight(x, y, myStone){
+      const enemy = myStone == "黒" ? "白" : "黒"
+      // y=8
+      if(x == 8 || array_table[x+1][y] != enemy){
+        return false
+      }
+      for(var i = x; i < 9 ; i++ ){
+        if(array_table[i][y] == myStone){
+          return true
+        }
+      }
+      return false
+    },
+    stoneReplaceLeftDown(x, y, myStone){
+      const enemy = myStone == "黒" ? "白" : "黒"
+      if(x == 1 || y == 8 || array_table[x-1][y+1] != enemy ){
+        return false
+      }
+      var i = x;
+      for(var j = y ; j < 9 ; j++ ){
+        if(array_table[i][j] == myStone ){
+          return true
+        }else if(i ==1 ){
+          return false
+        }
+        i--;
+      }
+      return false
+    },
+    stoneReplaceDown(x, y, myStone){
+      const enemy = myStone == "黒" ? "白" : "黒"
+      // x = 8
+      if(y == 8 || array_table[x][y+1] != enemy){
+        return false
+      }
+      for(var i = y; i < 9; i++){
+        if(array_table[x][i] == myStone){
+          return true
+        }
+      }
+      return false
+    },
+    stoneReplaceRightDown(x, y, myStone){
+      const enemy = myStone == "黒" ? "白" : "黒"
+      // x=8 y=8
+      if(x == 8 || y == 8 || array_table[x+1][y+1] != enemy ){
+        return false
+      }
+      var i = x;
+      for(var j = y ; j < 9 ; j++ ){
+        if(array_table[i][j] == myStone){
+          return true
+        }else if(i == 8){
+          return false
+        }
+        i++;
+      }
+      return false
+    }
   }
 }
 </script>
